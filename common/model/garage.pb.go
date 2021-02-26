367c712dd7dab42a157fc57e45ea1a95ea5089b5
Nasrul Faizin <faizinnasrul21@gmail.com>
2021-02-26 19:12:12 +0700

    style(model): change field naming

--- common/model/garage.pb.go
+++ common/model/garage.pb.go
@@ -73,7 +73,7 @@ func (m *GarageCoordinate) GetLongitude() float32 {
 }
 
 type Garage struct {
-	Id                   string            `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	ID                   string            `protobuf:"bytes,1,opt,name=ID,proto3" json:"ID,omitempty"`
 	Name                 string            `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
 	Coordinate           *GarageCoordinate `protobuf:"bytes,3,opt,name=coordinate,proto3" json:"coordinate,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
@@ -106,9 +106,9 @@ func (m *Garage) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Garage proto.InternalMessageInfo
 
-func (m *Garage) GetId() string {
+func (m *Garage) GetID() string {
 	if m != nil {
-		return m.Id
+		return m.ID
 	}
 	return ""
 }
@@ -205,86 +205,86 @@ func (m *GarageListByUser) GetList() map[string]*GarageList {
 	return nil
 }
 
-type GarageUserId struct {
-	UserId               string   `protobuf:"bytes,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+type GarageUserID struct {
+	UserID               string   `protobuf:"bytes,1,opt,name=userID,proto3" json:"userID,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *GarageUserId) Reset()         { *m = GarageUserId{} }
-func (m *GarageUserId) String() string { return proto.CompactTextString(m) }
-func (*GarageUserId) ProtoMessage()    {}
-func (*GarageUserId) Descriptor() ([]byte, []int) {
+func (m *GarageUserID) Reset()         { *m = GarageUserID{} }
+func (m *GarageUserID) String() string { return proto.CompactTextString(m) }
+func (*GarageUserID) ProtoMessage()    {}
+func (*GarageUserID) Descriptor() ([]byte, []int) {
 	return fileDescriptor_d35248283e3db3c1, []int{4}
 }
 
-func (m *GarageUserId) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_GarageUserId.Unmarshal(m, b)
+func (m *GarageUserID) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GarageUserID.Unmarshal(m, b)
 }
-func (m *GarageUserId) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_GarageUserId.Marshal(b, m, deterministic)
+func (m *GarageUserID) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GarageUserID.Marshal(b, m, deterministic)
 }
-func (m *GarageUserId) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GarageUserId.Merge(m, src)
+func (m *GarageUserID) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GarageUserID.Merge(m, src)
 }
-func (m *GarageUserId) XXX_Size() int {
-	return xxx_messageInfo_GarageUserId.Size(m)
+func (m *GarageUserID) XXX_Size() int {
+	return xxx_messageInfo_GarageUserID.Size(m)
 }
-func (m *GarageUserId) XXX_DiscardUnknown() {
-	xxx_messageInfo_GarageUserId.DiscardUnknown(m)
+func (m *GarageUserID) XXX_DiscardUnknown() {
+	xxx_messageInfo_GarageUserID.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_GarageUserId proto.InternalMessageInfo
+var xxx_messageInfo_GarageUserID proto.InternalMessageInfo
 
-func (m *GarageUserId) GetUserId() string {
+func (m *GarageUserID) GetUserID() string {
 	if m != nil {
-		return m.UserId
+		return m.UserID
 	}
 	return ""
 }
 
-type GarageAndUserId struct {
-	UserId               string   `protobuf:"bytes,1,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
+type GarageAndUserID struct {
+	UserID               string   `protobuf:"bytes,1,opt,name=userID,proto3" json:"userID,omitempty"`
 	Garage               *Garage  `protobuf:"bytes,2,opt,name=garage,proto3" json:"garage,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
 }
 
-func (m *GarageAndUserId) Reset()         { *m = GarageAndUserId{} }
-func (m *GarageAndUserId) String() string { return proto.CompactTextString(m) }
-func (*GarageAndUserId) ProtoMessage()    {}
-func (*GarageAndUserId) Descriptor() ([]byte, []int) {
+func (m *GarageAndUserID) Reset()         { *m = GarageAndUserID{} }
+func (m *GarageAndUserID) String() string { return proto.CompactTextString(m) }
+func (*GarageAndUserID) ProtoMessage()    {}
+func (*GarageAndUserID) Descriptor() ([]byte, []int) {
 	return fileDescriptor_d35248283e3db3c1, []int{5}
 }
 
-func (m *GarageAndUserId) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_GarageAndUserId.Unmarshal(m, b)
+func (m *GarageAndUserID) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GarageAndUserID.Unmarshal(m, b)
 }
-func (m *GarageAndUserId) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_GarageAndUserId.Marshal(b, m, deterministic)
+func (m *GarageAndUserID) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GarageAndUserID.Marshal(b, m, deterministic)
 }
-func (m *GarageAndUserId) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GarageAndUserId.Merge(m, src)
+func (m *GarageAndUserID) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GarageAndUserID.Merge(m, src)
 }
-func (m *GarageAndUserId) XXX_Size() int {
-	return xxx_messageInfo_GarageAndUserId.Size(m)
+func (m *GarageAndUserID) XXX_Size() int {
+	return xxx_messageInfo_GarageAndUserID.Size(m)
 }
-func (m *GarageAndUserId) XXX_DiscardUnknown() {
-	xxx_messageInfo_GarageAndUserId.DiscardUnknown(m)
+func (m *GarageAndUserID) XXX_DiscardUnknown() {
+	xxx_messageInfo_GarageAndUserID.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_GarageAndUserId proto.InternalMessageInfo
+var xxx_messageInfo_GarageAndUserID proto.InternalMessageInfo
 
-func (m *GarageAndUserId) GetUserId() string {
+func (m *GarageAndUserID) GetUserID() string {
 	if m != nil {
-		return m.UserId
+		return m.UserID
 	}
 	return ""
 }
 
-func (m *GarageAndUserId) GetGarage() *Garage {
+func (m *GarageAndUserID) GetGarage() *Garage {
 	if m != nil {
 		return m.Garage
 	}
@@ -297,37 +297,37 @@ func init() {
 	proto.RegisterType((*GarageList)(nil), "model.GarageList")
 	proto.RegisterType((*GarageListByUser)(nil), "model.GarageListByUser")
 	proto.RegisterMapType((map[string]*GarageList)(nil), "model.GarageListByUser.ListEntry")
-	proto.RegisterType((*GarageUserId)(nil), "model.GarageUserId")
-	proto.RegisterType((*GarageAndUserId)(nil), "model.GarageAndUserId")
+	proto.RegisterType((*GarageUserID)(nil), "model.GarageUserID")
+	proto.RegisterType((*GarageAndUserID)(nil), "model.GarageAndUserID")
 }
 
 func init() { proto.RegisterFile("garage.proto", fileDescriptor_d35248283e3db3c1) }
 
 var fileDescriptor_d35248283e3db3c1 = []byte{
-	// 362 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x92, 0xcf, 0x4e, 0xc2, 0x40,
-	0x10, 0xc6, 0x69, 0x0b, 0xc5, 0x0e, 0xa8, 0x38, 0x26, 0x40, 0xaa, 0x07, 0xd8, 0xc4, 0xc0, 0xa9,
-	0x98, 0x1a, 0x83, 0xf1, 0x86, 0x86, 0x18, 0x0d, 0x17, 0x9b, 0x78, 0x36, 0xc5, 0x5d, 0x9b, 0xc6,
-	0xd2, 0x25, 0xed, 0xd6, 0xa4, 0x0f, 0xe2, 0xfb, 0x9a, 0xee, 0x42, 0xb1, 0x70, 0xf0, 0xb6, 0x3b,
-	0xbf, 0xef, 0x9b, 0x7f, 0x19, 0x68, 0x07, 0x7e, 0xe2, 0x07, 0xcc, 0x59, 0x27, 0x5c, 0x70, 0x6c,
-	0xac, 0x38, 0x65, 0x91, 0x7d, 0x11, 0x70, 0x1e, 0x44, 0x6c, 0x22, 0x83, 0xcb, 0xec, 0x73, 0xc2,
-	0x56, 0x6b, 0x91, 0x2b, 0x0d, 0x59, 0x40, 0xe7, 0x49, 0x7a, 0x1e, 0x39, 0x4f, 0x68, 0x18, 0xfb,
-	0x82, 0xa1, 0x0d, 0x47, 0x91, 0x2f, 0x42, 0x91, 0x51, 0xd6, 0xd7, 0x06, 0xda, 0x58, 0xf7, 0xca,
-	0x3f, 0x5e, 0x82, 0x15, 0xf1, 0x38, 0x50, 0x50, 0x97, 0x70, 0x17, 0x20, 0x0c, 0x4c, 0x95, 0x0d,
-	0x4f, 0x40, 0x0f, 0xa9, 0x74, 0x5b, 0x9e, 0x1e, 0x52, 0x44, 0xa8, 0xc7, 0xfe, 0x4a, 0x59, 0x2c,
-	0x4f, 0xbe, 0x71, 0x0a, 0xf0, 0x51, 0x56, 0xed, 0x1b, 0x03, 0x6d, 0xdc, 0x72, 0x7b, 0x8e, 0x6c,
-	0xda, 0xd9, 0x6f, 0xca, 0xfb, 0x23, 0x25, 0x13, 0x00, 0xc5, 0x17, 0x61, 0x2a, 0x70, 0x08, 0xf5,
-	0x28, 0x4c, 0x45, 0x5f, 0x1b, 0x18, 0xe3, 0x96, 0x7b, 0x5c, 0x49, 0xe0, 0x49, 0x44, 0x7e, 0xb4,
-	0xed, 0x98, 0x85, 0xe3, 0x21, 0x7f, 0x4b, 0x59, 0x82, 0xb7, 0x15, 0xdf, 0xb0, 0xe2, 0xdb, 0xc9,
-	0x9c, 0xe2, 0x39, 0x8f, 0x45, 0x92, 0xab, 0x5c, 0xf6, 0x0b, 0x58, 0x65, 0x08, 0x3b, 0x60, 0x7c,
-	0xb1, 0x7c, 0x33, 0x67, 0xf1, 0xc4, 0x11, 0x34, 0xbe, 0xfd, 0x28, 0x53, 0x93, 0xb6, 0xdc, 0xb3,
-	0x83, 0xb4, 0x9e, 0xe2, 0xf7, 0xfa, 0x9d, 0x46, 0x46, 0xd0, 0x56, 0xa0, 0xa8, 0xf4, 0x4c, 0xb1,
-	0x07, 0xcd, 0x2c, 0x65, 0xc9, 0x7b, 0xb9, 0x3a, 0x33, 0x93, 0x80, 0xbc, 0xc2, 0xa9, 0x12, 0xce,
-	0x62, 0xfa, 0x8f, 0x16, 0xaf, 0xc0, 0x54, 0x67, 0xb0, 0x69, 0x61, 0x6f, 0x23, 0x1b, 0xe8, 0x0a,
-	0x68, 0xaa, 0x48, 0x8a, 0xd7, 0x50, 0x97, 0x9b, 0x3c, 0xaf, 0x28, 0x55, 0x1d, 0xfb, 0x70, 0x02,
-	0x52, 0xc3, 0x29, 0x18, 0x33, 0x4a, 0xb1, 0x5b, 0x61, 0x65, 0x6f, 0x76, 0xd7, 0x51, 0x37, 0xe7,
-	0x6c, 0x6f, 0xce, 0x99, 0x17, 0x37, 0x47, 0x6a, 0x4b, 0x53, 0x46, 0x6e, 0x7e, 0x03, 0x00, 0x00,
-	0xff, 0xff, 0xda, 0x61, 0x00, 0xfe, 0xaa, 0x02, 0x00, 0x00,
+	// 361 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x52, 0xdd, 0x4e, 0xea, 0x40,
+	0x10, 0xa6, 0x2d, 0xf4, 0x9c, 0x0e, 0x9c, 0x23, 0x8e, 0x09, 0x92, 0xea, 0x05, 0x6c, 0xa2, 0x72,
+	0xb5, 0x98, 0x1a, 0x83, 0xf1, 0x0e, 0x85, 0x18, 0x0c, 0x17, 0xa6, 0x89, 0x0f, 0x50, 0xec, 0xda,
+	0x34, 0x96, 0x2e, 0x69, 0xb7, 0x26, 0x7d, 0x10, 0xdf, 0xd7, 0x74, 0xb7, 0x14, 0x0b, 0x89, 0x77,
+	0xb3, 0xf3, 0xfd, 0xcc, 0x37, 0x9b, 0x81, 0x4e, 0xe0, 0x25, 0x5e, 0xc0, 0xe8, 0x26, 0xe1, 0x82,
+	0x63, 0x6b, 0xcd, 0x7d, 0x16, 0xd9, 0x67, 0x01, 0xe7, 0x41, 0xc4, 0xc6, 0xb2, 0xb9, 0xca, 0xde,
+	0xc7, 0x6c, 0xbd, 0x11, 0xb9, 0xe2, 0x90, 0x25, 0x74, 0x9f, 0xa4, 0xe6, 0x91, 0xf3, 0xc4, 0x0f,
+	0x63, 0x4f, 0x30, 0xb4, 0xe1, 0x6f, 0xe4, 0x89, 0x50, 0x64, 0x3e, 0xeb, 0x6b, 0x03, 0x6d, 0xa4,
+	0xbb, 0xd5, 0x1b, 0xcf, 0xc1, 0x8a, 0x78, 0x1c, 0x28, 0x50, 0x97, 0xe0, 0xae, 0x41, 0x18, 0x98,
+	0xca, 0x0d, 0xff, 0x83, 0xbe, 0x98, 0x49, 0xb5, 0xe5, 0xea, 0x8b, 0x19, 0x22, 0x34, 0x63, 0x6f,
+	0xad, 0x24, 0x96, 0x2b, 0x6b, 0x9c, 0x00, 0xbc, 0x55, 0x53, 0xfb, 0xc6, 0x40, 0x1b, 0xb5, 0x9d,
+	0x53, 0x2a, 0x43, 0xd3, 0xfd, 0x50, 0xee, 0x0f, 0x2a, 0x19, 0x03, 0x28, 0x7c, 0x19, 0xa6, 0x02,
+	0x87, 0xd0, 0x8c, 0xc2, 0x54, 0xf4, 0xb5, 0x81, 0x31, 0x6a, 0x3b, 0xff, 0x6a, 0x06, 0xae, 0x84,
+	0xc8, 0x97, 0xb6, 0x5d, 0xb3, 0x50, 0x3c, 0xe4, 0xaf, 0x29, 0x4b, 0xf0, 0xb6, 0xa6, 0x1b, 0xd6,
+	0x74, 0x3b, 0x1a, 0x2d, 0xca, 0x79, 0x2c, 0x92, 0x5c, 0x79, 0xd9, 0xcf, 0x60, 0x55, 0x2d, 0xec,
+	0x82, 0xf1, 0xc1, 0xf2, 0x72, 0xcf, 0xa2, 0xc4, 0x2b, 0x68, 0x7d, 0x7a, 0x51, 0xa6, 0x36, 0x6d,
+	0x3b, 0xc7, 0x07, 0xb6, 0xae, 0xc2, 0xef, 0xf5, 0x3b, 0x8d, 0x5c, 0x42, 0x47, 0x01, 0xc5, 0xa4,
+	0xc5, 0x0c, 0x7b, 0x60, 0x66, 0xb2, 0x2a, 0x1d, 0xcb, 0x17, 0x79, 0x81, 0x23, 0xc5, 0x9b, 0xc6,
+	0xfe, 0xef, 0x54, 0xbc, 0x00, 0x53, 0x1d, 0x41, 0x19, 0x60, 0xef, 0x3f, 0x4a, 0xd0, 0x11, 0xf0,
+	0x47, 0x75, 0x52, 0xbc, 0x86, 0xa6, 0xfc, 0xc7, 0x93, 0x1a, 0x53, 0x8d, 0xb1, 0x0f, 0xf3, 0x93,
+	0x06, 0x4e, 0xc0, 0x98, 0xfa, 0x3e, 0xf6, 0x6a, 0x58, 0x15, 0xcd, 0xee, 0x51, 0x75, 0x71, 0x74,
+	0x7b, 0x71, 0x74, 0x5e, 0x5c, 0x1c, 0x69, 0xac, 0x4c, 0xd9, 0xb9, 0xf9, 0x0e, 0x00, 0x00, 0xff,
+	0xff, 0xc1, 0x61, 0x6d, 0x06, 0xa8, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -342,8 +342,8 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type GaragesClient interface {
-	List(ctx context.Context, in *GarageUserId, opts ...grpc.CallOption) (*GarageList, error)
-	Add(ctx context.Context, in *GarageAndUserId, opts ...grpc.CallOption) (*empty.Empty, error)
+	List(ctx context.Context, in *GarageUserID, opts ...grpc.CallOption) (*GarageList, error)
+	Add(ctx context.Context, in *GarageAndUserID, opts ...grpc.CallOption) (*empty.Empty, error)
 }
 
 type garagesClient struct {
@@ -354,7 +354,7 @@ func NewGaragesClient(cc *grpc.ClientConn) GaragesClient {
 	return &garagesClient{cc}
 }
 
-func (c *garagesClient) List(ctx context.Context, in *GarageUserId, opts ...grpc.CallOption) (*GarageList, error) {
+func (c *garagesClient) List(ctx context.Context, in *GarageUserID, opts ...grpc.CallOption) (*GarageList, error) {
 	out := new(GarageList)
 	err := c.cc.Invoke(ctx, "/model.Garages/List", in, out, opts...)
 	if err != nil {
@@ -363,7 +363,7 @@ func (c *garagesClient) List(ctx context.Context, in *GarageUserId, opts ...grpc
 	return out, nil
 }
 
-func (c *garagesClient) Add(ctx context.Context, in *GarageAndUserId, opts ...grpc.CallOption) (*empty.Empty, error) {
+func (c *garagesClient) Add(ctx context.Context, in *GarageAndUserID, opts ...grpc.CallOption) (*empty.Empty, error) {
 	out := new(empty.Empty)
 	err := c.cc.Invoke(ctx, "/model.Garages/Add", in, out, opts...)
 	if err != nil {
@@ -374,18 +374,18 @@ func (c *garagesClient) Add(ctx context.Context, in *GarageAndUserId, opts ...gr
 
 // GaragesServer is the server API for Garages service.
 type GaragesServer interface {
-	List(context.Context, *GarageUserId) (*GarageList, error)
-	Add(context.Context, *GarageAndUserId) (*empty.Empty, error)
+	List(context.Context, *GarageUserID) (*GarageList, error)
+	Add(context.Context, *GarageAndUserID) (*empty.Empty, error)
 }
 
 // UnimplementedGaragesServer can be embedded to have forward compatible implementations.
 type UnimplementedGaragesServer struct {
 }
 
-func (*UnimplementedGaragesServer) List(ctx context.Context, req *GarageUserId) (*GarageList, error) {
+func (*UnimplementedGaragesServer) List(ctx context.Context, req *GarageUserID) (*GarageList, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method List not implemented")
 }
-func (*UnimplementedGaragesServer) Add(ctx context.Context, req *GarageAndUserId) (*empty.Empty, error) {
+func (*UnimplementedGaragesServer) Add(ctx context.Context, req *GarageAndUserID) (*empty.Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Add not implemented")
 }
 
@@ -394,7 +394,7 @@ func RegisterGaragesServer(s *grpc.Server, srv GaragesServer) {
 }
 
 func _Garages_List_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GarageUserId)
+	in := new(GarageUserID)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -406,13 +406,13 @@ func _Garages_List_Handler(srv interface{}, ctx context.Context, dec func(interf
 		FullMethod: "/model.Garages/List",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GaragesServer).List(ctx, req.(*GarageUserId))
+		return srv.(GaragesServer).List(ctx, req.(*GarageUserID))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
 func _Garages_Add_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GarageAndUserId)
+	in := new(GarageAndUserID)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -424,7 +424,7 @@ func _Garages_Add_Handler(srv interface{}, ctx context.Context, dec func(interfa
 		FullMethod: "/model.Garages/Add",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GaragesServer).Add(ctx, req.(*GarageAndUserId))
+		return srv.(GaragesServer).Add(ctx, req.(*GarageAndUserID))
 	}
 	return interceptor(ctx, in, info, handler)
 }
